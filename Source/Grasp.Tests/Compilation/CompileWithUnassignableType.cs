3e2653c67f3279b71b4b11004dc5e312ee41565a
Bryan Watts <bwatts@gmail.com>
2012-04-16 21:48:57 -0400

    Added compilation tests for common schema scenarios

---
+++ Source/Grasp.Tests/Compilation/CompileWithUnassignableType.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Cloak.NUnit;
+using NUnit.Framework;
+
+namespace Grasp.Compilation
+{
+	public class CompileWithUnassignableType : Behavior
+	{
+		Calculation _calculation;
+		GraspSchema _schema;
+		InvalidCalculationResultTypeException _exception;
+
+		protected override void Given()
+		{
+			_calculation = new Calculation(new Variable("Grasp", "Output", typeof(int)), Expression.Constant(""));
+
+			_schema = new GraspSchema(Enumerable.Empty<Variable>(), new[] { _calculation });
+		}
+
+		protected override void When()
+		{
+			try
+			{
+				_schema.Compile();
+			}
+			catch(InvalidCalculationResultTypeException ex)
+			{
+				_exception = ex;
+			}
+		}
+
+		[Then]
+		public void Throws()
+		{
+			Assert.That(_exception, Is.Not.Null);
+		}
+
+		[Then]
+		public void HasOriginalCalculation()
+		{
+			Assert.That(_exception.Calculation, Is.EqualTo(_calculation));
+		}
+	}
+}
---
+++ Source/Grasp.Tests/Compilation/CompileWithUnknownVariable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Cloak.NUnit;
+using NUnit.Framework;
+
+namespace Grasp.Compilation
+{
+	public class CompileWithUnknownVariable : Behavior
+	{
+		Calculation _calculation;
+		GraspSchema _schema;
+		InvalidCalculationVariableException _exception;
+
+		protected override void Given()
+		{
+			_calculation = new Calculation(new Variable("Grasp", "Output", typeof(int)), Variable.Expression(new Variable("Grasp", "Unknown", typeof(int))));
+
+			_schema = new GraspSchema(Enumerable.Empty<Variable>(), new[] { _calculation });
+		}
+
+		protected override void When()
+		{
+			try
+			{
+				_schema.Compile();
+			}
+			catch(InvalidCalculationVariableException ex)
+			{
+				_exception = ex;
+			}
+		}
+
+		[Then]
+		public void Throws()
+		{
+			Assert.That(_exception, Is.Not.Null);
+		}
+
+		[Then]
+		public void HasOriginalCalculation()
+		{
+			Assert.That(_exception.Calculation, Is.EqualTo(_calculation));
+		}
+	}
+}
