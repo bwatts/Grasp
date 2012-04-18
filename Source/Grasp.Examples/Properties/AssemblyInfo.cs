ebab5df3fd791edb998e2b294189a92541bfe06c
Bryan Watts <bwatts@gmail.com>
2012-04-18 00:36:19 -0400

    Renamed Grasp.Tests to Grasp.Examples

--- Source/Grasp.Examples/Properties/AssemblyInfo.cs
+++ Source/Grasp.Examples/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Grasp.Tests")]
+[assembly: AssemblyTitle("Grasp.Examples")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Grasp.Tests")]
+[assembly: AssemblyProduct("Grasp.Examples")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
