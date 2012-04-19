670b264650947eca176596752a5cd8e9b45e626c
Bryan Watts <bwatts@gmail.com>
2012-04-18 22:12:10 -0400

    Added solution-level assembly info and linked to each project. Set solution version to 0.1.0.0 and added corresponding NuGet package.

---
+++ Source/SolutionAssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Grasp")]
+[assembly: AssemblyCopyright("Copyright © 2012 Bryan Watts")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
