d0ea1171ed2c0d6853fde81322e680dd03eaed58
Bryan Watts <bwatts@gmail.com>
2012-04-19 23:16:23 -0400

    Added comments to VariableBinding

--- Source/Grasp/VariableBinding.cs
+++ Source/Grasp/VariableBinding.cs
@@ -6,8 +6,16 @@ using System.Text;
 
 namespace Grasp
 {
+	/// <summary>
+	/// A binding between a variable and its value in a particular context
+	/// </summary>
 	public class VariableBinding
 	{
+		/// <summary>
+		/// Initializes a binding with the specified variable and value
+		/// </summary>
+		/// <param name="variable">The bound variable</param>
+		/// <param name="value">The value to which the specified variable is bound</param>
 		public VariableBinding(Variable variable, object value)
 		{
 			Contract.Requires(variable != null);
@@ -18,8 +26,14 @@ namespace Grasp
 			Value = value;
 		}
 
+		/// <summary>
+		/// Gets the bound variable
+		/// </summary>
 		public Variable Variable { get; private set; }
 
+		/// <summary>
+		/// Gets the value to which <see cref="Variable"/> is bound
+		/// </summary>
 		public object Value { get; set; }
 	}
 }
