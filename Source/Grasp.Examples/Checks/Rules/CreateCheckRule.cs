c455d6ca934abafdd7a79915cebbac00f6e8dfb2
Bryan Watts <bwatts@gmail.com>
2012-04-21 14:58:37 -0400

    Updated Cloak reference to 0.4.1.0. Added tests for check rules.

---
+++ Source/Grasp.Examples/Checks/Rules/CreateCheckRule.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Cloak.NUnit;
+using Cloak.Reflection;
+using NUnit.Framework;
+
+namespace Grasp.Checks.Rules
+{
+	public class CreateCheckRule : Behavior
+	{
+		MethodInfo _method;
+		CheckRule _checkRule;
+
+		protected override void Given()
+		{
+			_method = Reflect.Func<ICheckable<int>, Check<int>>(Checkable.IsPositive);
+		}
+
+		protected override void When()
+		{
+			_checkRule = Rule.Check(_method);
+		}
+
+		[Then]
+		public void HasCheckType()
+		{
+			Assert.That(_checkRule.Type, Is.EqualTo(RuleType.Check));
+		}
+
+		[Then]
+		public void HasOriginalMethod()
+		{
+			Assert.That(_checkRule.Method, Is.EqualTo(_method));
+		}
+
+		[Then]
+		public void HasNoCheckArguments()
+		{
+			Assert.That(_checkRule.CheckArguments, Is.Empty);
+		}
+	}
+}
---
+++ Source/Grasp.Examples/Checks/Rules/CreateCheckRuleWithArguments.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Cloak.NUnit;
+using Cloak.Reflection;
+using NUnit.Framework;
+
+namespace Grasp.Checks.Rules
+{
+	public class CreateCheckRuleWithArguments : Behavior
+	{
+		string _value;
+		MethodInfo _method;
+		CheckRule _checkRule;
+
+		protected override void Given()
+		{
+			_value = "_";
+
+			_method = Reflect.Func<ICheckable<string>, string, Check<string>>(Checkable.StartsWith);
+		}
+
+		protected override void When()
+		{
+			_checkRule = Rule.Check(_method, _value);
+		}
+
+		[Then]
+		public void HasCheckType()
+		{
+			Assert.That(_checkRule.Type, Is.EqualTo(RuleType.Check));
+		}
+
+		[Then]
+		public void HasOriginalMethod()
+		{
+			Assert.That(_checkRule.Method, Is.EqualTo(_method));
+		}
+
+		[Then]
+		public void HasOriginalArguments()
+		{
+			Assert.That(_checkRule.CheckArguments.Single(), Is.EqualTo(_value));
+		}
+	}
+}
