521d71024df442e9d381ea75c593135b9735855a
Bryan Watts <bwatts@gmail.com>
2012-04-21 01:20:35 -0400

    Added the Checkable class and passes/fails methods

---
+++ Source/Grasp/Checks/Checkable.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+
+namespace Grasp.Checks
+{
+	/// <summary>
+	/// Provides a set of static methods for checking objects which implement <see cref="ICheckable{T}"/>
+	/// </summary>
+	public static class Checkable
+	{
+		#region Passes
+		/// <summary>
+		/// Checks whether the target data passes the specified check
+		/// </summary>
+		/// <typeparam name="T">The type of target data</typeparam>
+		/// <param name="check">The base check</param>
+		/// <param name="nextCheck">The check which should pass</param>
+		/// <returns>A check which applies the base and next checks</returns>
+		public static Check<T> Passes<T>(this ICheckable<T> check, Func<T, bool> nextCheck)
+		{
+			Contract.Requires(check != null);
+			Contract.Requires(nextCheck != null);
+
+			return new PassesCheck<T>(check, nextCheck);
+		}
+
+		/// <summary>
+		/// Checks whether the target data passed an already-applied check
+		/// </summary>
+		/// <typeparam name="T">The type of target data</typeparam>
+		/// <param name="check">The base check</param>
+		/// <param name="nextCheckResult">The result of the check which should pass</param>
+		/// <returns>A check which applies the base and next check result</returns>
+		public static Check<T> Passes<T>(this ICheckable<T> check, bool nextCheckResult)
+		{
+			Contract.Requires(check != null);
+
+			return new FixedPassesCheck<T>(check, nextCheckResult);
+		}
+
+		private sealed class PassesCheck<T> : Check<T>
+		{
+			private readonly ICheckable<T> _baseCheck;
+			private readonly Func<T, bool> _nextCheck;
+
+			internal PassesCheck(ICheckable<T> baseCheck, Func<T, bool> nextCheck)
+				: base(baseCheck.Target)
+			{
+				_baseCheck = baseCheck;
+				_nextCheck = nextCheck;
+			}
+
+			public override bool Apply()
+			{
+				return _baseCheck.Apply() && _nextCheck(Target);
+			}
+		}
+
+		private sealed class FixedPassesCheck<T> : Check<T>
+		{
+			private readonly ICheckable<T> _baseCheck;
+			private readonly bool _nextCheckResult;
+
+			internal FixedPassesCheck(ICheckable<T> baseCheck, bool nextCheckResult)
+				: base(baseCheck.Target)
+			{
+				_baseCheck = baseCheck;
+				_nextCheckResult = nextCheckResult;
+			}
+
+			public override bool Apply()
+			{
+				return _baseCheck.Apply() && _nextCheckResult;
+			}
+		}
+		#endregion
+
+		#region Fails
+		/// <summary>
+		/// Checks whether the target data fails the specified check
+		/// </summary>
+		/// <typeparam name="T">The type of target data</typeparam>
+		/// <param name="check">The base check</param>
+		/// <param name="nextCheck">The check which should fail</param>
+		/// <returns>A check which applies the base and next checks</returns>
+		public static Check<T> Fails<T>(this ICheckable<T> check, Func<T, bool> nextCheck)
+		{
+			Contract.Requires(check != null);
+			Contract.Requires(nextCheck != null);
+
+			return new FailsCheck<T>(check, nextCheck);
+		}
+
+		/// <summary>
+		/// Checks whether the target data failed an already-applied check
+		/// </summary>
+		/// <typeparam name="T">The type of target data</typeparam>
+		/// <param name="check">The base check</param>
+		/// <param name="nextCheckResult">The result of the check which should fail</param>
+		/// <returns>A check which applies the base and next check result</returns>
+		public static Check<T> Fails<T>(this ICheckable<T> check, bool nextCheckResult)
+		{
+			Contract.Requires(check != null);
+
+			return new FixedFailsCheck<T>(check, nextCheckResult);
+		}
+
+		private sealed class FailsCheck<T> : Check<T>
+		{
+			private readonly ICheckable<T> _baseCheck;
+			private readonly Func<T, bool> _nextCheck;
+
+			internal FailsCheck(ICheckable<T> baseCheck, Func<T, bool> nextCheck) : base(baseCheck.Target)
+			{
+				_baseCheck = baseCheck;
+				_nextCheck = nextCheck;
+			}
+
+			public override bool Apply()
+			{
+				return _baseCheck.Apply() && !_nextCheck(Target);
+			}
+		}
+
+		private sealed class FixedFailsCheck<T> : Check<T>
+		{
+			private readonly ICheckable<T> _baseCheck;
+			private readonly bool _nextCheckResult;
+
+			internal FixedFailsCheck(ICheckable<T> baseCheck, bool nextCheckResult) : base(baseCheck.Target)
+			{
+				_baseCheck = baseCheck;
+				_nextCheckResult = nextCheckResult;
+			}
+
+			public override bool Apply()
+			{
+				return _baseCheck.Apply() && !_nextCheckResult;
+			}
+		}
+		#endregion
+	}
+}
