5224c457ec2aacb6a4ac2aa31505d86beaef4dc4
Bryan Watts <bwatts@gmail.com>
2012-04-21 00:24:02 -0400

    Added Checks namespace and initial check elements

---
+++ Source/Grasp/Checks/Check.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grasp.Checks
+{
+	/// <summary>
+	/// Creates checks which apply to pieces of data
+	/// </summary>
+	public static class Check
+	{
+		/// <summary>
+		/// Gets a check which applies to the specified target data and whose result defaults to true
+		/// </summary>
+		/// <typeparam name="T">The type of data to which the check applies</typeparam>
+		/// <param name="target">The data to check</param>
+		/// <returns>A check which applies to the specified target data</returns>
+		public static ICheckable<T> That<T>(T target)
+		{
+			return new ThatCheck<T>(target, true);
+		}
+
+		/// <summary>
+		/// Gets a check which applies to the specified target data and whose result defaults to the specified value
+		/// </summary>
+		/// <typeparam name="T">The type of data to which the check applies</typeparam>
+		/// <param name="target">The data to check</param>
+		/// <param name="defaultResult">The default result of invoking the check</param>
+		/// <returns>A check which applies to the specified target data</returns>
+		public static ICheckable<T> That<T>(T target, bool defaultResult)
+		{
+			return new ThatCheck<T>(target, defaultResult);
+		}
+
+		private sealed class ThatCheck<T> : Check<T>
+		{
+			private readonly bool _defaultResult;
+
+			internal ThatCheck(T target, bool defaultResult) : base(target)
+			{
+				_defaultResult = defaultResult;
+			}
+
+			public override bool Apply()
+			{
+				return _defaultResult;
+			}
+		}
+	}
+
+	/// <summary>
+	/// A boolean-valued function applied to a piece of data. Implicitly converts to boolean.
+	/// </summary>
+	/// <typeparam name="T">The type of data to which the boolean-valued function is applied</typeparam>
+	public abstract class Check<T> : ICheckable<T>
+	{
+		/// <summary>
+		/// Applies the specified check
+		/// </summary>
+		/// <param name="invokedCheck">The check to apply</param>
+		/// <returns>The result of applying the specified check</returns>
+		public static implicit operator bool(Check<T> invokedCheck)
+		{
+			return invokedCheck.Apply();
+		}
+
+		/// <summary>
+		/// Initializes a check with the specified target data
+		/// </summary>
+		/// <param name="target">The targeted data</param>
+		protected Check(T target)
+		{
+			Target = target;
+		}
+
+		/// <summary>
+		/// Gets the type of targeted data
+		/// </summary>
+		public Type TargetType
+		{
+			get { return typeof(T); }
+		}
+
+		/// <summary>
+		/// Gets the targeted data
+		/// </summary>
+		public T Target { get; private set; }
+
+		object ICheckable.Target
+		{
+			get { return Target; }
+		}
+
+		/// <summary>
+		/// When implemented by a derived class, applies the boolean-valued function to the target data
+		/// </summary>
+		/// <returns>The result of applying the boolean-valued function to the target data</returns>
+		public abstract bool Apply();
+	}
+}
---
+++ Source/Grasp/Checks/ICheckable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+
+namespace Grasp.Checks
+{
+	/// <summary>
+	/// Describes a boolean-valued function applied to a piece of data
+	/// </summary>
+	public interface ICheckable
+	{
+		/// <summary>
+		/// Gets the type of targeted data
+		/// </summary>
+		Type TargetType { get; }
+
+		/// <summary>
+		/// Gets the targeted data
+		/// </summary>
+		object Target { get; }
+
+		/// <summary>
+		/// Applies the boolean-valued function to the target data
+		/// </summary>
+		/// <returns>The result of applying the boolean-valued function to the target data</returns>
+		bool Apply();
+	}
+
+	/// <summary>
+	/// Describes a boolean-valued function applied to a piece of data
+	/// </summary>
+	/// <typeparam name="T">The type of data to which the boolean-valued function is applied</typeparam>
+	public interface ICheckable<out T> : ICheckable
+	{
+		/// <summary>
+		/// Gets the targeted data
+		/// </summary>
+		new T Target { get; }
+	}
+}
