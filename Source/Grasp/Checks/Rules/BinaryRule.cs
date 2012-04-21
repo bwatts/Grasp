c294ac53d061f706eb15b7c148f328f79a7a11cf
Bryan Watts <bwatts@gmail.com>
2012-04-21 11:35:33 -0400

    Added binary rules

---
+++ Source/Grasp/Checks/Rules/BinaryRule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grasp.Checks.Rules
+{
+	/// <summary>
+	/// Represents a logical operation on the results of two rules
+	/// </summary>
+	public sealed class BinaryRule : Rule
+	{
+		private readonly RuleType _type;
+
+		internal BinaryRule(RuleType type, Rule left, Rule right)
+		{
+			_type = type;
+			Left = left;
+			Right = right;
+		}
+
+		/// <summary>
+		/// Gets the type of this binary rule
+		/// </summary>
+		public override RuleType Type
+		{
+			get { return _type; }
+		}
+
+		/// <summary>
+		/// Gets the left operand of the binary operation
+		/// </summary>
+		public Rule Left { get; private set; }
+
+		/// <summary>
+		/// Gets the right operand of the binary operation
+		/// </summary>
+		public Rule Right { get; private set; }
+	}
+}
--- Source/Grasp/Checks/Rules/Rule.cs
+++ Source/Grasp/Checks/Rules/Rule.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
+using Cloak;
 
 namespace Grasp.Checks.Rules
 {
@@ -65,6 +66,75 @@ namespace Grasp.Checks.Rules
 			Contract.Requires(lambda != null);
 
 			return new LambdaRule(lambda);
+		}
+		#endregion
+
+		#region Operators
+		/// <summary>
+		/// Creates a <see cref="Grasp.Checks.Rules.BinaryRule"/> that represents a bitwise AND operation on the results of the specified operands
+		/// </summary>
+		/// <param name="left">The left operand of the AND</param>
+		/// <param name="right">The right operand of the AND</param>
+		/// <returns>A <see cref="Grasp.Checks.Rules.BinaryRule"/> with a type of <see cref="RuleType.And"/> and the specified operands</returns>
+		public static BinaryRule And(Rule left, Rule right)
+		{
+			Contract.Requires(left != null);
+			Contract.Requires(right != null);
+
+			return new BinaryRule(RuleType.And, left, right);
+		}
+
+		/// <summary>
+		/// Creates a <see cref="Grasp.Checks.Rules.BinaryRule"/> that represents a bitwise OR operation on the results of the specified operands
+		/// </summary>
+		/// <param name="left">The left operand of the OR</param>
+		/// <param name="right">The right operand of the OR</param>
+		/// <returns>A <see cref="Grasp.Checks.Rules.BinaryRule"/> with a type of <see cref="RuleType.Or"/> and the specified operands</returns>
+		public static BinaryRule Or(Rule left, Rule right)
+		{
+			Contract.Requires(left != null);
+			Contract.Requires(right != null);
+
+			return new BinaryRule(RuleType.Or, left, right);
+		}
+
+		/// <summary>
+		/// Creates a <see cref="Grasp.Checks.Rules.BinaryRule"/> that represents a bitwise XOR operation on the results of the specified operands
+		/// </summary>
+		/// <param name="left">The left operand of the XOR</param>
+		/// <param name="right">The right operand of the XOR</param>
+		/// <returns>A <see cref="Grasp.Checks.Rules.BinaryRule"/> with a type of <see cref="RuleType.ExclusiveOr"/> and the specified operands</returns>
+		public static BinaryRule ExclusiveOr(Rule left, Rule right)
+		{
+			Contract.Requires(left != null);
+			Contract.Requires(right != null);
+
+			return new BinaryRule(RuleType.ExclusiveOr, left, right);
+		}
+
+		/// <summary>
+		/// Creates a <see cref="Grasp.Checks.Rules.BinaryRule"/>, given the left and right operands, by calling the appropriate factory method
+		/// </summary>
+		/// <param name="binaryType">The type of the binary operation</param>
+		/// <param name="left">The left operand of the binary operation</param>
+		/// <param name="right">The right operand of the binary operation</param>
+		/// <returns>A <see cref="Grasp.Checks.Rules.BinaryRule"/> with the specified type and operands</returns>
+		public static BinaryRule MakeBinary(RuleType binaryType, Rule left, Rule right)
+		{
+			Contract.Requires(left != null);
+			Contract.Requires(right != null);
+
+			switch(binaryType)
+			{
+				case RuleType.And:
+					return And(left, right);
+				case RuleType.Or:
+					return Or(left, right);
+				case RuleType.ExclusiveOr:
+					return ExclusiveOr(left, right);
+				default:
+					throw new ArgumentOutOfRangeException("binaryType", Resources.NotBinaryType.FormatInvariant(binaryType));
+			}
 		}
 		#endregion
 
