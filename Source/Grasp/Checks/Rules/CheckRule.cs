82344aa883329af3dd17d1acefc246a342f0834c
Bryan Watts <bwatts@gmail.com>
2012-04-21 11:01:36 -0400

    Added check rules

---
+++ Source/Grasp/Checks/Rules/CheckRule.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Grasp.Checks.Rules
+{
+	/// <summary>
+	/// Represents a call to a check method
+	/// </summary>
+	public sealed class CheckRule : Rule
+	{
+		internal CheckRule(MethodInfo method, ReadOnlyCollection<object> checkArguments)
+		{
+			Method = method;
+			CheckArguments = checkArguments;
+		}
+
+		/// <summary>
+		/// Gets <see cref="RuleType.Check"/>
+		/// </summary>
+		public override RuleType Type
+		{
+			get { return RuleType.Check; }
+		}
+
+		/// <summary>
+		/// Gets the check method
+		/// </summary>
+		public MethodInfo Method { get; private set; }
+
+		/// <summary>
+		/// Gets the arguments to the check method (without the base check)
+		/// </summary>
+		public ReadOnlyCollection<object> CheckArguments { get; private set; }
+	}
+}
--- Source/Grasp/Checks/Rules/Rule.cs
+++ Source/Grasp/Checks/Rules/Rule.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Grasp.Checks.Rules
@@ -10,6 +12,36 @@ namespace Grasp.Checks.Rules
 	/// </summary>
 	public abstract class Rule
 	{
+		#region Check
+		/// <summary>
+		/// Creates a <see cref="Grasp.Checks.Rules.CheckRule"/> that represents a call to a check method
+		/// </summary>
+		/// <param name="method">The method which applies the check</param>
+		/// <param name="checkArguments">The arguments to the check method (without the base check)</param>
+		/// <returns>A <see cref="Grasp.Checks.Rules.CheckRule"/> with the specified method and check arguments</returns>
+		public static CheckRule Check(MethodInfo method, IEnumerable<object> checkArguments)
+		{
+			Contract.Requires(method != null);
+			Contract.Requires(checkArguments != null);
+
+			return new CheckRule(method, checkArguments.ToList().AsReadOnly());
+		}
+
+		/// <summary>
+		/// Creates a <see cref="Grasp.Checks.Rules.CheckRule"/> that represents a call to a check method
+		/// </summary>
+		/// <param name="method">The method which applies the check</param>
+		/// <param name="checkArguments">The arguments to the check method (without the base check)</param>
+		/// <returns>A <see cref="Grasp.Checks.Rules.CheckRule"/> with the specified method and check arguments</returns>
+		public static CheckRule Check(MethodInfo method, params object[] checkArguments)
+		{
+			Contract.Requires(method != null);
+			Contract.Requires(checkArguments != null);
+
+			return Check(method, checkArguments as IEnumerable<object>);
+		}
+		#endregion
+
 		/// <summary>
 		/// When implemented by a derived class, gets the type of this node in the tree
 		/// </summary>
