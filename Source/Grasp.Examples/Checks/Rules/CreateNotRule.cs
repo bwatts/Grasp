8c7286b20b09d7ef55be8914c3d0df5030212cf8
Bryan Watts <bwatts@gmail.com>
2012-04-21 15:20:09 -0400

    Added tests for not rules

---
+++ Source/Grasp.Examples/Checks/Rules/CreateNotRule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Cloak.NUnit;
+using NUnit.Framework;
+
+namespace Grasp.Checks.Rules
+{
+	public class CreateNotRule : Behavior
+	{
+		Rule _rule;
+		NotRule _notRule;
+
+		protected override void Given()
+		{
+			_rule = Rule.Constant(true);
+		}
+
+		protected override void When()
+		{
+			_notRule = Rule.Not(_rule);
+		}
+
+		[Then]
+		public void HasNotType()
+		{
+			Assert.That(_notRule.Type, Is.EqualTo(RuleType.Not));
+		}
+
+		[Then]
+		public void HasOriginalRule()
+		{
+			Assert.That(_notRule.Rule, Is.EqualTo(_rule));
+		}
+	}
+}
