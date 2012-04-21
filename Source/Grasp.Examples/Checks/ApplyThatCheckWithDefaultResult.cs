9ea6e3393702c2497f242060c5cc85cc82a45f04
Bryan Watts <bwatts@gmail.com>
2012-04-21 12:36:00 -0400

    Added check tests

---
+++ Source/Grasp.Examples/Checks/ApplyThatCheckWithDefaultResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Cloak.NUnit;
+using NUnit.Framework;
+
+namespace Grasp.Checks
+{
+	public class ApplyThatCheckWithDefaultResult : Behavior
+	{
+		bool _defaultResult;
+		ICheckable<int> _thatCheck;
+		bool _result;
+
+		protected override void Given()
+		{
+			_defaultResult = false;
+
+			_thatCheck = Check.That(1, _defaultResult);
+		}
+
+		protected override void When()
+		{
+			_result = _thatCheck.Apply();
+		}
+
+		[Then]
+		public void ResultIsDefault()
+		{
+			Assert.That(_result, Is.EqualTo(_defaultResult));
+		}
+	}
+}
---
+++ Source/Grasp.Examples/Checks/CreateThatCheck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Cloak.NUnit;
+using NUnit.Framework;
+
+namespace Grasp.Checks
+{
+	public class CreateThatCheck : Behavior
+	{
+		int _target;
+		ICheckable<int> _thatCheck;
+
+		protected override void Given()
+		{
+			_target = 1;
+		}
+
+		protected override void When()
+		{
+			_thatCheck = Check.That(_target);
+		}
+
+		[Then]
+		public void IsNotNull()
+		{
+			Assert.That(_thatCheck, Is.Not.Null);
+		}
+
+		[Then]
+		public void HasOriginalTarget()
+		{
+			Assert.That(_thatCheck.Target, Is.EqualTo(_target));
+		}
+
+		[Then]
+		public void HasTargetType()
+		{
+			Assert.That(_thatCheck.TargetType, Is.EqualTo(_target.GetType()));
+		}
+	}
+}
