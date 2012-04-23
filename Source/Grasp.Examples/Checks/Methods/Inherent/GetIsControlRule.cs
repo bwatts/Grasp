74d3efa4628865cb3aeaca0647a5c08df0f81a8a
Bryan Watts <bwatts@gmail.com>
2012-04-23 19:47:33 -0400

    Added check attribute for control characters

---
+++ Source/Grasp.Examples/Checks/Methods/Inherent/GetIsControlRule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Cloak.NUnit;
+using Cloak.Reflection;
+using Grasp.Checks.Rules;
+using NUnit.Framework;
+
+namespace Grasp.Checks.Methods.Inherent
+{
+	public class GetIsControlRule : Behavior
+	{
+		IsControlMethod _method;
+		MethodInfo _expectedMethod;
+		CheckRule _rule;
+
+		protected override void Given()
+		{
+			_method = new IsControlMethod();
+
+			_expectedMethod = Reflect.Func<ICheckable<char>, Check<char>>(Checkable.IsControl);
+		}
+
+		protected override void When()
+		{
+			_rule = _method.GetRule(typeof(char));
+		}
+
+		[Then]
+		public void HasIsNullMethod()
+		{
+			Assert.That(_rule.Method, Is.EqualTo(_expectedMethod));
+		}
+	}
+}
---
+++ Source/Grasp/Checks/Annotation/CheckIsControlAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasp.Checks.Methods;
+
+namespace Grasp.Checks.Annotation
+{
+	/// <summary>
+	/// Checks that the target <see cref="System.Char"/> is a control character
+	/// </summary>
+	public sealed class CheckIsControlAttribute : CheckAttribute
+	{
+		/// <summary>
+		/// Gets an instance of <see cref="IsControlMethod"/>
+		/// </summary>
+		/// <returns>An instance of <see cref="IsControlMethod"/></returns>
+		public override ICheckMethod GetCheckMethod()
+		{
+			return new IsControlMethod();
+		}
+	}
+}
