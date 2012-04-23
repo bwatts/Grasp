f71eaa16f22ed80a203799f1b32feaaeb9f07c29
Bryan Watts <bwatts@gmail.com>
2012-04-23 19:56:32 -0400

    Added check attributes for high surrogate characters

---
+++ Source/Grasp.Examples/Checks/Methods/Inherent/GetIsHighSurrogateRuleNonChar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Cloak.NUnit;
+using NUnit.Framework;
+
+namespace Grasp.Checks.Methods.Inherent
+{
+	public class GetIsHighSurrogateRuleNonChar : Behavior
+	{
+		IsHighSurrogateMethod _method;
+		GraspException _exception;
+
+		protected override void Given()
+		{
+			_method = new IsHighSurrogateMethod();
+		}
+
+		protected override void When()
+		{
+			try
+			{
+				_method.GetRule(typeof(string));
+			}
+			catch(GraspException ex)
+			{
+				_exception = ex;
+			}
+		}
+
+		[Then]
+		public void Throws()
+		{
+			Assert.That(_exception, Is.Not.Null);
+		}
+	}
+}
