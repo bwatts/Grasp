b866850ebb3e86436a0eee9ee4c8b38514bbd5d6
Bryan Watts <bwatts@gmail.com>
2012-04-23 20:56:17 -0400

    Added check attributes for non null or empty strings

---
+++ Source/Grasp/Checks/Annotation/CheckIsNotNullOrEmptyAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasp.Checks.Methods;
+
+namespace Grasp.Checks.Annotation
+{
+	/// <summary>
+	/// Checks that the target <see cref="System.String"/> is not null or empty
+	/// </summary>
+	public sealed class CheckIsNotNullOrEmptyAttribute : CheckAttribute
+	{
+		/// <summary>
+		/// Gets an instance of <see cref="IsNotNullOrEmptyMethod"/>
+		/// </summary>
+		/// <returns>An instance of <see cref="IsNotNullOrEmptyMethod"/></returns>
+		public override ICheckMethod GetCheckMethod()
+		{
+			return new IsNotNullOrEmptyMethod();
+		}
+	}
+}
