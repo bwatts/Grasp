9183e46bcb1a3db3a7d5deb5eed8419d93fda04e
Bryan Watts <bwatts@gmail.com>
2012-04-23 20:16:29 -0400

    Added check attributes for separator and surrogate characters

---
+++ Source/Grasp/Checks/Annotation/CheckIsSeparatorAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasp.Checks.Methods;
+
+namespace Grasp.Checks.Annotation
+{
+	/// <summary>
+	/// Checks that the target <see cref="System.Char"/> is a separator
+	/// </summary>
+	public sealed class CheckIsSeparatorAttribute : CheckAttribute
+	{
+		/// <summary>
+		/// Gets an instance of <see cref="IsSeparatorMethod"/>
+		/// </summary>
+		/// <returns>An instance of <see cref="IsSeparatorMethod"/></returns>
+		public override ICheckMethod GetCheckMethod()
+		{
+			return new IsSeparatorMethod();
+		}
+	}
+}
---
+++ Source/Grasp/Checks/Annotation/CheckIsSurrogateAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasp.Checks.Methods;
+
+namespace Grasp.Checks.Annotation
+{
+	/// <summary>
+	/// Checks that the target <see cref="System.Char"/> is a surrogate
+	/// </summary>
+	public sealed class CheckIsSurrogateAttribute : CheckAttribute
+	{
+		/// <summary>
+		/// Gets an instance of <see cref="IsSurrogateMethod"/>
+		/// </summary>
+		/// <returns>An instance of <see cref="IsSurrogateMethod"/></returns>
+		public override ICheckMethod GetCheckMethod()
+		{
+			return new IsSurrogateMethod();
+		}
+	}
+}
