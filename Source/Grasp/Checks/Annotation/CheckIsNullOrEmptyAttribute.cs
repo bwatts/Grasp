eb6778deeb829c537ccd3e57e64c7fa15399e375
Bryan Watts <bwatts@gmail.com>
2012-04-23 20:53:12 -0400

    Added check attributes for null or empty strings

---
+++ Source/Grasp/Checks/Annotation/CheckIsNullOrEmptyAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasp.Checks.Methods;
+
+namespace Grasp.Checks.Annotation
+{
+	/// <summary>
+	/// Checks that the target <see cref="System.String"/> is null or empty
+	/// </summary>
+	public sealed class CheckIsNullOrEmptyAttribute : CheckAttribute
+	{
+		/// <summary>
+		/// Gets an instance of <see cref="IsNullOrEmptyMethod"/>
+		/// </summary>
+		/// <returns>An instance of <see cref="IsNullOrEmptyMethod"/></returns>
+		public override ICheckMethod GetCheckMethod()
+		{
+			return new IsNullOrEmptyMethod();
+		}
+	}
+}
---
+++ Source/Grasp/Checks/Methods/IsNullOrEmptyMethod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Cloak.Reflection;
+
+namespace Grasp.Checks.Methods
+{
+	/// <summary>
+	/// Produces check rules for the <see cref="Checkable.IsNullOrEmpty"/> method
+	/// </summary>
+	public sealed class IsNullOrEmptyMethod : SingleTypeCheckMethod
+	{
+		/// <summary>
+		/// Gets <see cref="System.String"/>
+		/// </summary>
+		protected override Type TargetType
+		{
+			get { return typeof(string); }
+		}
+
+		/// <summary>
+		/// Gets the <see cref="Checkable.IsNullOrEmpty"/> method
+		/// </summary>
+		/// <param name="checkType">The type of check</param>
+		/// <returns>The <see cref="Checkable.IsNullOrEmpty"/> method</returns>
+		protected override MethodInfo GetMethod(Type checkType)
+		{
+			return Reflect.Func<ICheckable<string>, Check<string>>(Checkable.IsNullOrEmpty);
+		}
+	}
+}
