81fb6892379a6f82e503b6160e7a5c89f50e9ef7
Bryan Watts <bwatts@gmail.com>
2012-04-23 20:09:52 -0400

    Added check attributes for low surrogate characters

---
+++ Source/Grasp.Examples/Checks/Methods/Inherent/Characters/GetIsLowSurrogateRuleNonChar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Cloak.NUnit;
+using NUnit.Framework;
+
+namespace Grasp.Checks.Methods.Inherent.Characters
+{
+	public class GetIsLowSurrogateRuleNonChar : Behavior
+	{
+		IsLowSurrogateMethod _method;
+		GraspException _exception;
+
+		protected override void Given()
+		{
+			_method = new IsLowSurrogateMethod();
+		}
+
+		protected override void When()
+		{
+			try
+			{
+				_method.GetRule(typeof(string));
+			}
+			catch(GraspException ex)
+			{
+				_exception = ex;
+			}
+		}
+
+		[Then]
+		public void Throws()
+		{
+			Assert.That(_exception, Is.Not.Null);
+		}
+	}
+}
---
+++ Source/Grasp/Checks/Annotation/CheckIsLowSurrogateAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasp.Checks.Methods;
+
+namespace Grasp.Checks.Annotation
+{
+	/// <summary>
+	/// Checks that the target <see cref="System.Char"/> is a low surrogate
+	/// </summary>
+	public sealed class CheckIsLowSurrogateAttribute : CheckAttribute
+	{
+		/// <summary>
+		/// Gets an instance of <see cref="IsLowSurrogateMethod"/>
+		/// </summary>
+		/// <returns>An instance of <see cref="IsLowSurrogateMethod"/></returns>
+		public override ICheckMethod GetCheckMethod()
+		{
+			return new IsLowSurrogateMethod();
+		}
+	}
+}
