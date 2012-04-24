610bd1f37930827183c97f13ccdb57c85bac3945
Bryan Watts <bwatts@gmail.com>
2012-04-23 20:50:42 -0400

    Added check attributes non-empty strings

---
+++ Source/Grasp.Examples/Checks/Methods/Inherent/Strings/GetIsNotEmptyRule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Cloak.NUnit;
+using Cloak.Reflection;
+using Grasp.Checks.Rules;
+using NUnit.Framework;
+
+namespace Grasp.Checks.Methods.Inherent.Strings
+{
+	public class GetIsNotEmptyRule : Behavior
+	{
+		IsNotEmptyMethod _method;
+		MethodInfo _expectedMethod;
+		CheckRule _rule;
+
+		protected override void Given()
+		{
+			_method = new IsNotEmptyMethod();
+
+			_expectedMethod = Reflect.Func<ICheckable<string>, Check<string>>(Checkable.IsNotEmpty);
+		}
+
+		protected override void When()
+		{
+			_rule = _method.GetRule(typeof(string));
+		}
+
+		[Then]
+		public void HasExpectedMethod()
+		{
+			Assert.That(_rule.Method, Is.EqualTo(_expectedMethod));
+		}
+	}
+}
---
+++ Source/Grasp/Checks/Annotation/CheckIsNotEmptyAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasp.Checks.Methods;
+
+namespace Grasp.Checks.Annotation
+{
+	/// <summary>
+	/// Checks that the target <see cref="System.String"/> is not empty
+	/// </summary>
+	public sealed class CheckIsNotEmptyAttribute : CheckAttribute
+	{
+		/// <summary>
+		/// Gets an instance of <see cref="IsNotEmptyMethod"/>
+		/// </summary>
+		/// <returns>An instance of <see cref="IsNotEmptyMethod"/></returns>
+		public override ICheckMethod GetCheckMethod()
+		{
+			return new IsNotEmptyMethod();
+		}
+	}
+}
