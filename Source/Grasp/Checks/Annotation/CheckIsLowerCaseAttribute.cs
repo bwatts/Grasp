40f6dac80d36bcb55cfed46aeda1b4c988f44e3a
Bryan Watts <bwatts@gmail.com>
2012-04-23 20:07:37 -0400

    Added check attributes for lower-case letters

---
+++ Source/Grasp/Checks/Annotation/CheckIsLowerCaseAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasp.Checks.Methods;
+
+namespace Grasp.Checks.Annotation
+{
+	/// <summary>
+	/// Checks that the target <see cref="System.Char"/> is lower-case
+	/// </summary>
+	public sealed class CheckIsLowerCaseAttribute : CheckAttribute
+	{
+		/// <summary>
+		/// Gets an instance of <see cref="IsLowerCaseMethod"/>
+		/// </summary>
+		/// <returns>An instance of <see cref="IsLowerCaseMethod"/></returns>
+		public override ICheckMethod GetCheckMethod()
+		{
+			return new IsLowerCaseMethod();
+		}
+	}
+}
