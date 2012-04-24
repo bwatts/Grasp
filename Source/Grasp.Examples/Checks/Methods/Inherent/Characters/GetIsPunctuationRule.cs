83ae41b9942ec18eafec25e728ea09810d11357b
Bryan Watts <bwatts@gmail.com>
2012-04-23 20:12:11 -0400

    Added check attributes for punctuation

---
+++ Source/Grasp.Examples/Checks/Methods/Inherent/Characters/GetIsPunctuationRule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Cloak.NUnit;
+using Cloak.Reflection;
+using Grasp.Checks.Rules;
+using NUnit.Framework;
+
+namespace Grasp.Checks.Methods.Inherent.Characters
+{
+	public class GetIsPunctuationRule : Behavior
+	{
+		IsPunctuationMethod _method;
+		MethodInfo _expectedMethod;
+		CheckRule _rule;
+
+		protected override void Given()
+		{
+			_method = new IsPunctuationMethod();
+
+			_expectedMethod = Reflect.Func<ICheckable<char>, Check<char>>(Checkable.IsPunctuation);
+		}
+
+		protected override void When()
+		{
+			_rule = _method.GetRule(typeof(char));
+		}
+
+		[Then]
+		public void HasIsNullMethod()
+		{
+			Assert.That(_rule.Method, Is.EqualTo(_expectedMethod));
+		}
+	}
+}
---
+++ Source/Grasp/Checks/Annotation/CheckIsPunctuationAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasp.Checks.Methods;
+
+namespace Grasp.Checks.Annotation
+{
+	/// <summary>
+	/// Checks that the target <see cref="System.Char"/> is punctuation
+	/// </summary>
+	public sealed class CheckIsPunctuationAttribute : CheckAttribute
+	{
+		/// <summary>
+		/// Gets an instance of <see cref="IsPunctuationMethod"/>
+		/// </summary>
+		/// <returns>An instance of <see cref="IsPunctuationMethod"/></returns>
+		public override ICheckMethod GetCheckMethod()
+		{
+			return new IsPunctuationMethod();
+		}
+	}
+}
