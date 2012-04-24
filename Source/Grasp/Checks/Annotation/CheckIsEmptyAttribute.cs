e4a17bd2eeff55447adb6e2f9f010bbf830a411f
Bryan Watts <bwatts@gmail.com>
2012-04-23 20:47:29 -0400

    Added check attributes for empty strings

---
+++ Source/Grasp/Checks/Annotation/CheckIsEmptyAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasp.Checks.Methods;
+
+namespace Grasp.Checks.Annotation
+{
+	/// <summary>
+	/// Checks that the target <see cref="System.String"/> is empty
+	/// </summary>
+	public sealed class CheckIsEmptyAttribute : CheckAttribute
+	{
+		/// <summary>
+		/// Gets an instance of <see cref="IsEmptyMethod"/>
+		/// </summary>
+		/// <returns>An instance of <see cref="IsEmptyMethod"/></returns>
+		public override ICheckMethod GetCheckMethod()
+		{
+			return new IsEmptyMethod();
+		}
+	}
+}
