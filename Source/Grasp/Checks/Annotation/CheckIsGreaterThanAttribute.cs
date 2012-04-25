c8edb9720ad647861218bfbeb276ab3ed2813e24
Bryan Watts <bwatts@gmail.com>
2012-04-24 23:01:38 -0400

    Added check attributes for greater than comparisons

---
+++ Source/Grasp/Checks/Annotation/CheckIsGreaterThanAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasp.Checks.Methods;
+
+namespace Grasp.Checks.Annotation
+{
+	/// <summary>
+	/// Checks that the target value is greater than a specified value
+	/// </summary>
+	public sealed class CheckIsGreaterThanAttribute : CheckAttribute
+	{
+		/// <summary>
+		/// Initializes an attribute with the specified value
+		/// </summary>
+		/// <param name="value">The value to compare</param>
+		public CheckIsGreaterThanAttribute(object value)
+		{
+			Value = value;
+		}
+
+		/// <summary>
+		/// Gets the value to compare
+		/// </summary>
+		public object Value { get; private set; }
+
+		/// <summary>
+		/// Gets an instance of <see cref="IsGreaterThanMethod"/>
+		/// </summary>
+		/// <returns>An instance of <see cref="IsGreaterThanMethod"/></returns>
+		public override ICheckMethod GetCheckMethod()
+		{
+			return new IsGreaterThanMethod(Value);
+		}
+	}
+}
---
+++ Source/Grasp/Checks/Methods/IsGreaterThanMethod.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Cloak.Reflection;
+
+namespace Grasp.Checks.Methods
+{
+	/// <summary>
+	/// Produces check rules for the <see cref="Checkable.IsGreaterThan{T}"/> method
+	/// </summary>
+	public sealed class IsGreaterThanMethod : ComparisonMethod
+	{
+		private readonly object _value;
+
+		/// <summary>
+		/// Initializes a method with the specified value
+		/// </summary>
+		/// <param name="value">The value to compare</param>
+		public IsGreaterThanMethod(object value)
+		{
+			_value = value;
+		}
+
+		/// <summary>
+		/// Gets the <see cref="Checkable.IsGreaterThan{T}"/> method
+		/// </summary>
+		/// <param name="targetType">The type of target data to which this check method is applied</param>
+		/// <param name="checkType">The type of check</param>
+		/// <returns>The <see cref="Checkable.IsGreaterThan{T}"/> method</returns>
+		protected override MethodInfo GetMethod(Type targetType, Type checkType)
+		{
+			return typeof(Checkable).GetMethod("IsGreaterThan").MakeGenericMethod(targetType);
+		}
+
+		/// <summary>
+		/// Gets the value to compare
+		/// </summary>
+		/// <param name="targetType">The type of target data to which this check method is applied</param>
+		/// <returns>The value to compare</returns>
+		protected override IEnumerable<object> GetCheckArguments(Type targetType)
+		{
+			yield return _value;
+		}
+	}
+}
