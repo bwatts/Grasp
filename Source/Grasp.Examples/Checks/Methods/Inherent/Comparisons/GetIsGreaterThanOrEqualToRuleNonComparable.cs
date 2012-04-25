7fef57788ec40ea23f53abc73a855374eb00935f
Bryan Watts <bwatts@gmail.com>
2012-04-24 22:57:12 -0400

    Added check attributes for greater than or equal to checks

---
+++ Source/Grasp.Examples/Checks/Methods/Inherent/Comparisons/GetIsGreaterThanOrEqualToRuleNonComparable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Cloak.NUnit;
+using Cloak.Reflection;
+using Grasp.Checks.Rules;
+using NUnit.Framework;
+
+namespace Grasp.Checks.Methods.Inherent.Comparisons
+{
+	public class GetIsGreaterThanOrEqualToRuleNonComparable : Behavior
+	{
+		IsGreaterThanOrEqualToMethod _method;
+		GraspException _exception;
+
+		protected override void Given()
+		{
+			_method = new IsGreaterThanOrEqualToMethod(0);
+		}
+
+		protected override void When()
+		{
+			try
+			{
+				_method.GetRule(typeof(NonComparable));
+			}
+			catch(GraspException ex)
+			{
+				_exception = ex;
+			}
+		}
+
+		[Then]
+		public void Throws()
+		{
+			Assert.That(_exception, Is.Not.Null);
+		}
+
+		private class NonComparable
+		{}
+	}
+}
---
+++ Source/Grasp/Checks/Annotation/CheckIsGreaterThanOrEqualToAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasp.Checks.Methods;
+
+namespace Grasp.Checks.Annotation
+{
+	/// <summary>
+	/// Checks that the target value is greater than or equal to a specified value
+	/// </summary>
+	public sealed class CheckIsGreaterThanOrEqualToAttribute : CheckAttribute
+	{
+		/// <summary>
+		/// Initializes an attribute with the specified value
+		/// </summary>
+		/// <param name="value">The value to compare</param>
+		public CheckIsGreaterThanOrEqualToAttribute(object value)
+		{
+			Value = value;
+		}
+
+		/// <summary>
+		/// Gets the value to compare
+		/// </summary>
+		public object Value { get; private set; }
+
+		/// <summary>
+		/// Gets an instance of <see cref="IsGreaterThanOrEqualToMethod"/>
+		/// </summary>
+		/// <returns>An instance of <see cref="IsGreaterThanOrEqualToMethod"/></returns>
+		public override ICheckMethod GetCheckMethod()
+		{
+			return new IsGreaterThanOrEqualToMethod(Value);
+		}
+	}
+}
