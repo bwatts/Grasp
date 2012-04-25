b1074da92219bfdedc98ff4d4dafc866d2a686b2
Bryan Watts <bwatts@gmail.com>
2012-04-25 18:56:27 -0400

    Added checks for ZIP code format

---
+++ Source/Grasp/Checks/Annotation/CheckIsZipCodeAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasp.Checks.Methods;
+
+namespace Grasp.Checks.Annotation
+{
+	/// <summary>
+	/// Checks that the target <see cref="System.String"/> is a ZIP code
+	/// </summary>
+	public sealed class CheckIsZipCodeAttribute : CheckAttribute
+	{
+		/// <summary>
+		/// Gets an instance of <see cref="IsZipCodeMethod"/>
+		/// </summary>
+		/// <returns>An instance of <see cref="IsZipCodeMethod"/></returns>
+		public override ICheckMethod GetCheckMethod()
+		{
+			return new IsZipCodeMethod();
+		}
+	}
+}
