d5edb8b6b41542b5a70f87318525121a4e7426e8
Bryan Watts <bwatts@gmail.com>
2012-04-25 18:54:09 -0400

    Added check attributes for social security format

---
+++ Source/Grasp/Checks/Annotation/CheckIsSocialSecurityNumberAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasp.Checks.Methods;
+
+namespace Grasp.Checks.Annotation
+{
+	/// <summary>
+	/// Checks that the target <see cref="System.String"/> is a social security number
+	/// </summary>
+	public sealed class CheckIsSocialSecurityNumberAttribute : CheckAttribute
+	{
+		/// <summary>
+		/// Gets an instance of <see cref="IsSocialSecurityNumberMethod"/>
+		/// </summary>
+		/// <returns>An instance of <see cref="IsSocialSecurityNumberMethod"/></returns>
+		public override ICheckMethod GetCheckMethod()
+		{
+			return new IsSocialSecurityNumberMethod();
+		}
+	}
+}
