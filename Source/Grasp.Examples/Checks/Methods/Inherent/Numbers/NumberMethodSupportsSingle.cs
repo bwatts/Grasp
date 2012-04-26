b96677d35bd478fba01cd72625e4b2a9878c1873
Bryan Watts <bwatts@gmail.com>
2012-04-25 22:23:59 -0400

    Added number check methods

---
+++ Source/Grasp.Examples/Checks/Methods/Inherent/Numbers/NumberMethodSupportsSingle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Cloak.NUnit;
+using Cloak.Reflection;
+using Grasp.Checks.Rules;
+using NUnit.Framework;
+
+namespace Grasp.Checks.Methods.Numbers
+{
+	public class NumberMethodSupportsSingle : Behavior
+	{
+		TestNumberCheckMethod _method;
+		bool _isSupported;
+
+		protected override void Given()
+		{
+			_method = new TestNumberCheckMethod();
+		}
+
+		protected override void When()
+		{
+			_isSupported = _method.SupportsType();
+		}
+
+		[Then]
+		public void TypeIsSupported()
+		{
+			Assert.That(_isSupported);
+		}
+
+		private sealed class TestNumberCheckMethod : GraspNumberCheckMethod
+		{
+			internal bool SupportsType()
+			{
+				return SupportsTargetType(typeof(float));
+			}
+
+			protected override string MethodName
+			{
+				get { throw new NotImplementedException(); }
+			}
+		}
+	}
+}
---
+++ Source/Grasp/Checks/Methods/GraspNumberCheckMethod.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Grasp.Checks.Methods
+{
+	/// <summary>
+	/// Base implementation of a method defined by Grasp which applies to numbers
+	/// </summary>
+	[ContractClass(typeof(GraspNumberCheckMethodContract))]
+	public abstract class GraspNumberCheckMethod : MultiTypeCheckMethod
+	{
+		internal GraspNumberCheckMethod()
+		{}
+
+		/// <summary>
+		/// Gets <see cref="System.Decimal"/>, <see cref="System.Double"/>, <see cref="System.Single"/>, <see cref="System.Int32"/>, and <see cref="System.Int64"/>
+		/// </summary>
+		protected override IEnumerable<Type> TargetTypes
+		{
+			get
+			{
+				yield return typeof(decimal);
+				yield return typeof(double);
+				yield return typeof(float);
+				yield return typeof(int);
+				yield return typeof(long);
+			}
+		}
+
+		/// <summary>
+		/// Gets the method which corresponds to <see cref="MethodName"/>
+		/// </summary>
+		/// <param name="targetType">The type of target data to which this check method is applied</param>
+		/// <param name="checkType">The type of check</param>
+		/// <returns>The method which corresponds to <see cref="MethodName"/></returns>
+		protected override MethodInfo GetMethod(Type targetType, Type checkType)
+		{
+			return typeof(Checkable)
+				.GetMethods(BindingFlags.Public | BindingFlags.Static)
+				.Where(method => method.Name == MethodName)
+				.Where(method => method.GetParameters().First().ParameterType == checkType)
+				.First();
+		}
+
+		/// <summary>
+		/// When implemented by a derived class, gets the name of the applicable method
+		/// </summary>
+		protected abstract string MethodName { get; }
+	}
+
+	[ContractClassFor(typeof(GraspNumberCheckMethod))]
+	internal abstract class GraspNumberCheckMethodContract : GraspNumberCheckMethod
+	{
+		protected override string MethodName
+		{
+			get
+			{
+				Contract.Ensures(!String.IsNullOrEmpty(Contract.Result<string>()));
+
+				return null;
+			}
+		}
+	}
+}
