79ca6ed92f1e3f83f2a1115bcf64f904be744f44
Bryan Watts <bwatts@gmail.com>
2012-04-25 23:03:33 -0400

    Added check attributes for adjusted percentages

---
+++ Source/Grasp/Checks/Annotation/CheckIsAdjustedPercentageAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grasp.Checks.Methods;
+
+namespace Grasp.Checks.Annotation
+{
+	/// <summary>
+	/// Checks that the target <see cref="System.Decimal"/>, <see cref="System.Double"/>, or <see cref="System.Single"/> is an adjusted percentage
+	/// </summary>
+	public sealed class CheckIsAdjustedPercentageAttribute : CheckAttribute
+	{
+		/// <summary>
+		/// Gets an instance of <see cref="IsAdjustedPercentageMethod"/>
+		/// </summary>
+		/// <returns>An instance of <see cref="IsAdjustedPercentageMethod"/></returns>
+		public override ICheckMethod GetCheckMethod()
+		{
+			return new IsAdjustedPercentageMethod();
+		}
+	}
+}
---
+++ Source/Grasp/Checks/Methods/IsAdjustedPercentageMethod.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Cloak.Reflection;
+
+namespace Grasp.Checks.Methods
+{
+	/// <summary>
+	/// Produces check rules for the <see cref="Checkable.IsAdjustedPercentage(ICheckable{decimal})"/>, <see cref="Checkable.IsAdjustedPercentage(ICheckable{double})"/>,
+	/// and <see cref="Checkable.IsAdjustedPercentage(ICheckable{float})"/> methods
+	/// </summary>
+	public sealed class IsAdjustedPercentageMethod : GraspNumberCheckMethod
+	{
+		/// <summary>
+		/// Gets <see cref="System.Decimal"/>, <see cref="System.Double"/>, and <see cref="System.Single"/>
+		/// </summary>
+		protected override IEnumerable<Type> TargetTypes
+		{
+			get
+			{
+				yield return typeof(decimal);
+				yield return typeof(double);
+				yield return typeof(float);
+			}
+		}
+
+		/// <summary>
+		/// Gets "IsAdjustedPercentage"
+		/// </summary>
+		protected override string MethodName
+		{
+			get { return "IsAdjustedPercentage"; }
+		}
+	}
+}
