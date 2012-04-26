771598827976f2635f37d5ded70fbe3cd5392031
Bryan Watts <bwatts@gmail.com>
2012-04-25 23:43:15 -0400

    Added tests that numeric check methods do not support non-numeric types

---
+++ Source/Grasp.Examples/Checks/Methods/Inherent/Numbers/GetIsNotPositiveRuleNonNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Cloak.NUnit;
+using Cloak.Reflection;
+using Grasp.Checks.Rules;
+using NUnit.Framework;
+
+namespace Grasp.Checks.Methods.Numbers
+{
+	public class GetIsNotPositiveRuleNonNumber : Behavior
+	{
+		IsNotPositiveMethod _method;
+		GraspException _exception;
+
+		protected override void Given()
+		{
+			_method = new IsNotPositiveMethod();
+		}
+
+		protected override void When()
+		{
+			try
+			{
+				_method.GetRule(typeof(string));
+			}
+			catch(GraspException ex)
+			{
+				_exception = ex;
+			}
+		}
+
+		[Then]
+		public void Throws()
+		{
+			Assert.That(_exception, Is.Not.Null);
+		}
+	}
+}
