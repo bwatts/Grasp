692ee27d2ef2f55a00576cc557d9195b1d579004
Bryan Watts <bwatts@gmail.com>
2012-04-29 18:08:27 -0400

    Created expression trees from constant rules

---
+++ Source/Grasp.Examples/Checks/Rules/LambdaExpressions/ConstantToLambda.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using Cloak.NUnit;
+using NUnit.Framework;
+
+namespace Grasp.Checks.Rules.LambdaExpressions
+{
+	public class ConstantToLambda : Behavior
+	{
+		bool _value;
+		ConstantRule _constantRule;
+		Type _targetType;
+		LambdaExpression _lambda;
+
+		protected override void Given()
+		{
+			_value = true;
+
+			_constantRule = Rule.Constant(_value);
+
+			_targetType = typeof(int);
+		}
+
+		protected override void When()
+		{
+			_lambda = _constantRule.ToLambdaExpression(_targetType);
+		}
+
+		[Then]
+		public void HasOneParameter()
+		{
+			Assert.That(_lambda.Parameters.Count, Is.EqualTo(1));
+		}
+
+		[Then]
+		public void ParameterIsTargetType()
+		{
+			var parameter = _lambda.Parameters.Single();
+
+			Assert.That(parameter.Type, Is.EqualTo(_targetType));
+		}
+
+		[Then]
+		public void ParameterNameIsTarget()
+		{
+			var parameter = _lambda.Parameters.Single();
+
+			Assert.That(parameter.Name, Is.EqualTo("target"));
+		}
+
+		[Then]
+		public void BodyIsConstant()
+		{
+			Assert.That(_lambda.Body, Is.InstanceOf<ConstantExpression>());
+		}
+
+		[Then]
+		public void BodyHasConstantValue()
+		{
+			var constant = (ConstantExpression) _lambda.Body;
+
+			Assert.That(constant.Value, Is.EqualTo(_value));
+		}
+	}
+}
--- Source/Grasp/Checks/Rules/ConvertRuleToLambdaExpression.cs
+++ Source/Grasp/Checks/Rules/ConvertRuleToLambdaExpression.cs
@@ -8,9 +8,48 @@ namespace Grasp.Checks.Rules
 {
 	internal sealed class ConvertRuleToLambdaExpression : RuleVisitor
 	{
+		ParameterExpression _targetParameter;
+		Expression _body;
+
 		internal LambdaExpression ConvertToLambdaExpression(Rule rule, Type targetType)
 		{
-			throw new NotImplementedException();
+			_targetParameter = Expression.Parameter(targetType, "target");
+
+			Visit(rule);
+
+			return Expression.Lambda(_body, _targetParameter);
+		}
+
+		protected override Rule VisitCheck(CheckRule node)
+		{
+			return node;
+		}
+
+		protected override Rule VisitConstant(ConstantRule node)
+		{
+			_body = Expression.Constant(node.Passes);
+
+			return node;
+		}
+
+		protected override Rule VisitLambda(LambdaRule node)
+		{
+			return node;
+		}
+
+		protected override Rule VisitBinary(BinaryRule node)
+		{
+			return node;
+		}
+
+		protected override Rule VisitNot(NotRule node)
+		{
+			return node;
+		}
+
+		protected override Rule VisitMember(MemberRule node)
+		{
+			return node;
 		}
 	}
 }
