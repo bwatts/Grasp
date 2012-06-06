f0917123e85878de42fec68a2b8548561e21361b
Bryan Watts <bwatts@gmail.com>
2012-06-06 03:33:43 -0400

    Added HTTP links and meshes as well as resource meshes

---
+++ Source/Grasp.Hypermedia.Http/IHttpMesh.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grasp.Hypermedia.Http
+{
+	public interface IHttpMesh
+	{
+		HttpLink GetHttpLink(Link link);
+	}
+}
