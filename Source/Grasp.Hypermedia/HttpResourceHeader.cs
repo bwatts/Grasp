61d246c22f60791041b82d1fee06d6508f428f7e
Bryan Watts <bwatts@gmail.com>
2012-11-08 16:17:03 -0500

    Added missing field for base link

--- Source/Grasp.Hypermedia/HttpResourceHeader.cs
+++ Source/Grasp.Hypermedia/HttpResourceHeader.cs
@@ -10,18 +10,22 @@ namespace Grasp.Hypermedia
 	public class HttpResourceHeader : Notion
 	{
 		public static readonly Field<string> TitleField = Field.On<HttpResourceHeader>.For(x => x.Title);
+		public static readonly Field<Hyperlink> BaseLinkField = Field.On<HttpResourceHeader>.For(x => x.BaseLink);
 		public static readonly Field<ManyInOrder<Hyperlink>> LinksField = Field.On<HttpResourceHeader>.For(x => x.Links);
 
 		public HttpResourceHeader(string title, Hyperlink baseLink, IEnumerable<Hyperlink> links)
 		{
 			Contract.Requires(title != null);
+			Contract.Requires(baseLink != null);
 			Contract.Requires(links != null);
 
 			Title = title;
+			BaseLink = baseLink;
 			Links = new ManyInOrder<Hyperlink>(links);
 		}
 
 		public string Title { get { return GetValue(TitleField); } private set { SetValue(TitleField, value); } }
+		public Hyperlink BaseLink { get { return GetValue(BaseLinkField); } private set { SetValue(BaseLinkField, value); } }
 		public ManyInOrder<Hyperlink> Links { get { return GetValue(LinksField); } private set { SetValue(LinksField, value); } }
 	}
 }
